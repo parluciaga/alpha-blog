b809aa538c83a21311c56bfd2da3fd065851c780
Patricio Arluciaga <parluciaga@zoho.com>
2020-05-03 02:55:54 +0000

    Added articles index feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,10 @@ class ArticlesController < ApplicationController
   def show
     byebug
     @article = Article.find(params[:id])
-    
+  end
+  
+  def index
+    @articles = Article.all
   end
 
 end
